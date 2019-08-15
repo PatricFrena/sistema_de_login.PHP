7d842cee7267d93ac309b9f0983c300289ccd0d4
PatricFrena <patric_frena@estudante.sc.senai.br>
2019-08-15 14:57:55 -0300

    Ajax formulário de login

--- Index.php
+++ Index.php
@@ -25,9 +25,7 @@
         <section class="row">
             <div class="col-lg-4 offset-lg-4" id="alerta">
                 <div class="alert alert-success text-center">
-                    <strong id="resultado">
-                        Maravilhoso mundo sem o Sublime!
-                    </strong>
+                    <strong id="resultado"></strong>
                 </div>
             </div>
         </section>
@@ -36,7 +34,7 @@
         <section class="row mb-5">
             <div class="col-lg-4 offset-lg-4 bg-light rounded" id="caixaLogin">
                 <h2 class="text-center mt-2">Entrada no sistema</h2>
-                <form action="#" id="formLogin" class="p-2">
+                <form id="formLogin" class="p-2">
 
                     <div class="form-group">
                         <input type="text" name="nomeUsuario" id="nomeUsuario" class="form-control" placeholder="Nome do usuário" minlength="5" required>
@@ -165,6 +163,30 @@
         /* jQuery */
         $(function() {
 
+            //Preparação dos dados para o back-end
+            //Envio dos dados do formulário de login
+            $('#btnEntrar').click(function(e) {
+                let formLogin = document.querySelector("#formLogin");
+                if (formLogin.checkValidity()) {
+                    e.preventDefault(); //Não recarregar a página
+                    $.ajax({
+                        url: 'recebe.php',
+                        method: 'post',
+                        data: $('#formLogin').serialize() + '&action=login',
+                        success: function(resposta) {
+                            $('#alerta').show();
+                            $('#resultado').html(resposta);
+                        }
+                    });
+                }
+            });
+
+            //Formulário de Cadastro de usuário 
+            $("#btnRegistrar").click(function(e) {});
+
+            //Formulário para mudar de senha
+            $("#btnEnviarEmail").click(function(e) {});
+
             //Trocar da Tela de Login para Recuperar Senha
             $("#btnEsqueci").click(function() {
                 $("#caixaLogin").hide();
@@ -229,7 +251,6 @@
             max: jQuery.validator.format("Por favor, forne&ccedil;a um valor menor ou igual a {0}."),
             min: jQuery.validator.format("Por favor, forne&ccedil;a um valor maior ou igual a {0}.")
         });
-        
     </script>
 </body>
 
