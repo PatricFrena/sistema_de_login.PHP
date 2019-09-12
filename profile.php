d6571d4e2f6fff2b5719a891382dc502e73fa7ba
patricfrena <patric_frena@estudante.sc.senai.br>
2019-09-12 16:41:45 -0300

    preparação para o sistema de recuperação de senha
    #12

--- profile.php
+++ profile.php
@@ -1,5 +1,5 @@
 <?php
-//Protegendo profile.php só para usuários logados
+//Protegendo profile.php só para usuário logados
 require_once 'session.php'
 ?>
 <!DOCTYPE html>
@@ -12,24 +12,23 @@ require_once 'session.php'
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-
     <title>Perfil de <?= $usuario ?></title>
 </head>
 
 <body class="bg-dark">
 
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">FrenaYT</a>
+        <a class="navbar-brand" href="#">FrenaYTs</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Alterna navegação">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(Página atual)</span></a>
+                    <a class="nav-link" href="profile.php">Home <span class="sr-only">(Página atual)</span></a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Link 1</a>
+                    <a class="nav-link" href="#">link 1</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Link 2</a>
@@ -39,39 +38,42 @@ require_once 'session.php'
                         <?= $usuario ?>
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <a class="dropdown-item" href="#">Configurações</a>
-                        <a class="dropdown-item" href="sair.php">Sair</a>
-                        <a class="dropdown-item" href="#">Link 3</a>
+                        <a class="dropdown-item" href="slider.php">Fotos</a>
+                        <a class="dropdown-item" href="#">Configuração</a>
+                        <a class="dropdown-item" href="sair.php">sair</a>
+
                     </div>
                 </li>
             </ul>
         </div>
     </nav>
 
-<main class="container mt-4">   
-</div>
-    <div class="row no-gutters">
-        <div class="col-6 col-md-4">
-            <div id="imagem">
-                <img src="<?= $imagem ?>"style="width:200px; height:200px;">
+
+
+    <main class="container mt-4">
+        </div>>
+        <div class="row no-gutters">
+            <div class="col-6 col-md-4">
+                <div id="imagem">
+                    <img src=" <?= $imagem ?> " style="width:200px; height:200px; ">
+                </div>
             </div>
-        </div>
-        <div class="col-12 col-sm-6 col-md-8">
-            <h1 class="text-light">Perfil De Usuario</h1>
-            <h2 class="text-light">Nome:<?= $nome ?></h2>
-            <h2 class="text-light">E-mail:<?= $email ?></h2>
-            <h2 class="text-light">Data de Cadastro: <?= $dataCriacao ?></h2>
-        </div>
-</main>
+            <div class="col-12 col-sm-6 col-md-8">
+                <h1 class="text-light">Perfil De Usuario</h1>
+                <h2 class="text-light">Nome:<?= $nome ?></h2>
+                <h2 class="text-light">E-mail:<?= $email ?></h2>
+                <h2 class="text-light">Data de Cadastro: <?= $dataCriacao ?></h2>
+            </div>
+    </main>
 
+    </main>
 
+    <!-- Opcional JavaScript  -->
+    <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
+    <script src=" https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
-
-        <!-- JavaScript (Opcional) -->
-        <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </body>
 
 </html>
